85470d3b367b48177232a45eb4691bcae2a812f4
niudix_pc <ndxndx163@163.com>
2022-10-20 20:56:53 +0800

    加了个二维数组的函数

--- scource_10_20_V2.c
+++ scource_10_20_V2.c
@@ -4,9 +4,14 @@
 1）各班平均分、最高分、最高分学生学号
 2)最高分学生班级、学生在该班级中的序号及最高分
 3）平均分最高的班级及平均成绩
+*/
 
+/*
+	Version 2 增加了一个成绩输入的函数 void input
 */
 # include<stdio.h>
+
+void Input(int* score,int number_of_class,int number_of_stu);
 int main()
 {
 	int score[3][10] = { 89,79,96,88,68,77,92,85,76,93,74,85,84,91,86,94,75,96,74,98,85,97,85,67,89,86,91,87,83,91 };
@@ -52,4 +57,18 @@ int main()
 	printf("%d班的平均分最高，为%d分\n", max_all_total_score[0]+1, max_all_total_score[1]/10);
 
 	return 0;
+}
+
+void Input(int* score, int number_of_class, int number_of_stu)
+{
+	int i, j;
+
+	for (i = 0 ; i < number_of_class; i++)
+	{
+		for (j = 0 ; j < number_of_stu; j++)
+		{
+			scanf("%d", &score[i][j]);
+		}
+	}
+
 }
